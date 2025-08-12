a9bd090bf1fa9a83dc8ca33f25dd060aa7027eee
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-13 04:49:11 +0500

    Math Expressions

---
+++ Day-2-Math-Expressions/script.js
@@ -0,0 +1,24 @@
+// q1. Do numbers ka multiplication aur division result print karo.
+// const num1 = 10;
+// const num2 = 20;
+// console.log(`Multiplication : ${num1 * num2}`);
+// console.log(`Division : ${num1 / num2}`);
+
+// q2. Ek number ka square aur cube nikal ke print karo.
+// let num = 2;
+// console.log(`Square : ${num ** num}`);
+// console.log(`Cube : ${num ** 3}`);
+
+// q3. Math.sqrt() ka use karke ek number ka square root print karo.
+// console.log(Math.sqrt(4));
+
+// q4. Ek random number 1 se 100 ke beech generate karo.
+// const randomNumber = Math.floor( Math.random() * 100 + 1);
+// console.log(randomNumber);
+
+// q5. Decimal number ko round, floor, aur ceil karke print karo.
+// const decimaNumber = 3.4;
+// console.log(`Floor : ${Math.floor(decimaNumber)}`);
+// console.log(`Ceil : ${Math.ceil(decimaNumber)}`);
+// console.log(`Round : ${Math.round(decimaNumber)}`);
+
