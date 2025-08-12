1c96f5415cdf6c9d7a3d0047f834959c1a90ee6f
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-12 15:31:29 +0500

    Variables and data types

---
+++ Day-1-variables-and-datatypes/script.js
@@ -0,0 +1,26 @@
+// q1. Ek variable name me apna naam store karo aur console pe print karo.
+
+let myName = "Tousif Ahmad";
+// console.log(myName);
+
+// q2. Do numbers ka sum nikal ke result variable me store karo.
+const num1 = 10;
+const num2 = 20;
+const result = num1 + num2;
+// console.log(result);
+
+// q3. Ek string me apni favorite movie ka naam rakho aur uska length print karo.
+const favMovie = "Mission Impossible";
+// console.log(favMovie.length);
+
+// q4. Ek variable me temperature rakho aur uska type print karo.
+const temperature = 45;
+// console.log(typeof temperature);
+
+// q5. Do alag variables ka type check karo aur compare karo.
+const firstName = "Tousif";
+const age = 22;
+// console.log(typeof myName);
+// console.log(typeof age);
+
+
