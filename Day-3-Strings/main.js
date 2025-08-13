c5c23b8b79adc491a0607466c644df50f4595749
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-13 17:11:34 +0500

    strings

---
+++ Day-3-Strings/main.js
@@ -0,0 +1,19 @@
+// q1. Ek string ko uppercase me convert karo.
+// const myName = "Tousif Ahmad";
+// console.log(myName.toUpperCase());
+
+// q2. Ek string ko lowercase me convert karo.
+// const myName = "Tousif Ahmad";
+// console.log(myName.toLowerCase());
+
+// q3. Ek string ke first 5 characters print karo.
+// const js = "javascript is fun";
+// console.log(js.slice(0, 5));
+
+// q4. Ek sentence me ek word ko replace karo.
+// const sentence = "hey i am tousif";
+// console.log(sentence.replace("tousif", "Sam"));
+
+// q5. Ek string ke beech se ek word extract karo.
+// const str = "this is string";
+// console.log(str.slice(0,4));
