53046172eb49bbb3d2f268f4dd5943f801fbee94
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-15 16:06:08 +0500

    user Input (Prompt)

---
+++ Day-4-Prompt/main.js
@@ -0,0 +1,18 @@
+// q1. User se naam lo aur greeting print karo.
+// const userName = prompt("Enter your name");
+// console.log("hello " + userName);
+
+// q2. User se 2 numbers lo aur unka sum print karo.
+// const num1 = +prompt("Enter first number ");
+// const num2 = +prompt("Enter second number ");
+// console.log("sum = ", num1 + num2);
+
+// q3. User se birth year lo aur age calculate karo.
+// const birthYear = +prompt("Enter your birth year");
+// console.log(2025 - birthYear);
+
+// q4.User se ek sentence lo aur usme total characters count karo.
+// const sentence = prompt("Enter a sentence");
+// console.log(sentence.length);
+
+// q5.
