fb959527161d328b61c2a73f5066cedc2126f11d
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-18 16:20:14 +0500

    if else

---
+++ day-5-if-else/main.js
@@ -0,0 +1,47 @@
+// q1. Ek number positive, negative, ya zero hai ye check karo.
+// let num = 2;
+// if (num === 0){
+//     console.log("Zero");
+// }
+// else if (num > 0){
+//     console.log("Positive");
+// }
+// else{
+//     console.log("Negative");
+// }
+
+// q2. Age check karo aur agar 18+ ho to “Adult” print karo, warna “Minor”.
+// const age = 23;
+// if (age >= 18){
+//     console.log("Adult");
+// }
+// else{
+//     console.log("Minor");
+// }
+
+// q3. Password check karo – agar sahi hai to “Access Granted”, warna “Denied”.
+// let Password = "sam123";
+// if (Password === "sam123"){
+//     console.log("Access granted");
+// }
+// else{
+//     console.log("Denied");
+// }
+
+// q4. Temperature ke hisaab se “Hot” ya “Cold” print karo.
+// const Temperature = 45;
+// if (Temperature > 40){
+//     console.log("Hot");
+// }
+// else{
+//     console.log("Cold");
+// }
+
+// q5. Ek number even ya odd hai ye check karo.
+// const number = 10;
+// if (number % 2 === 0){
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd");
+// }
