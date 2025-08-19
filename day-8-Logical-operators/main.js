72a2bacaf7293416fc491f6fb2a0e224ec837a11
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-19 15:23:41 +0500

    Logical operators

---
+++ day-8-Logical-operators/main.js
@@ -0,0 +1,45 @@
+// q1. Check karo ki ek number 10 aur 50 ke beech hai.
+// let num = 28;
+// if (num >= 10 && num <= 50){
+//     console.log("The number between 10 and 50");
+// }
+
+// q2. Check karo ki ek person student hai aur fee paid hai.
+// let isStudent = true;
+// let feePaid = true;
+// if (isStudent && feePaid){
+//     console.log("You can sit on exam");
+// }
+
+// q3. Ek variable ka value ya to "red" ya "blue" hai ye check karo.
+// const color = 'red';
+// if (color === "red" || color === "blue"){
+//     console.log("Color is either red or blue");
+// }
+// else{
+//     console.log("Color is neither red nor blue");
+// }
+
+// q4. 3 numbers me se sabse bada number print karo.
+// const num1 = 2;
+// const num2 = 4;
+// const num3 = 5;
+
+// if (num1 > num2 && num1 > num3){
+//     console.log(`Num1 ${num1} is greater`);
+// }
+// else if (num2 > num1 && num2 > num3){
+//     console.log(`Num2 ${num2} is greater`);
+// }
+// else{
+//     console.log(`Num3 ${num3} is greater`);
+// }
+
+// q5. Weather check – agar raining hai ya cloudy hai to umbrella le jao.
+const weather = "rainy";
+if (weather === "rainy" || weather === "cloudy"){
+    console.log("Umbrella le jao");
+} 
+else{
+    console.log("aise he jao");
+}
