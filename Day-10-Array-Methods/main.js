598fac7c6e1b762ffe8cb49f7a9443d92bc1db8d
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-20 15:32:52 +0500

    Array Methods

---
+++ Day-10-Array-Methods/main.js
@@ -0,0 +1,25 @@
+// q1. Ek array ko reverse karo.
+// const nums = [1,2,3,4,5,"a"];
+// console.log(nums.reverse());
+
+// q2. Ek array ko sort karo.
+// const nums = [100,11,12,3,4,5,1 ];
+// nums.sort((a,b)=>{
+//     return a - b;
+// })
+// console.log(nums);
+
+// q3. Ek array ke elements ko join karke string banao.
+// const arr = ["ali", "khan", "sam"];
+// const str = arr.join("_");
+// console.log(str);
+
+// q4. slice ka use karke ek part nikal lo.
+// const arr = ["ali", "khan", "sam"];
+// console.log(arr.slice(0,2));
+
+// q5. splice ka use karke ek element delete aur add karo.
+const nums = [100,11,12,3,4,5,1 ];
+nums.splice(1,1,8)
+// console.log(nums.splice(1,1,8));
+console.log(nums);
