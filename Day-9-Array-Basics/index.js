ca652a91678291c8606b8b2d0fd8254fac9e221f
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-20 15:10:38 +0500

    Array Basics

---
+++ Day-9-Array-Basics/index.js
@@ -0,0 +1,22 @@
+// q1. Apne 5 friends ke naam ek array me rakho aur print karo.
+// const friends = ["shafqat", "Hamad", "furqan", "Akmal", "Farman"];
+// console.log(friends);
+
+// q2. Ek array ka first aur last element print karo.
+// const nums = [1,2,3,4,5,6];
+// console.log(nums[0]);
+// console.log(nums[5]);
+
+// q3. Ek array me ek naya item add karo.
+// const heroes = ["spiderman", "Batman", "Ironman"];
+// heroes.push("Superman");
+// console.log(heroes);
+
+// q4. Ek array ka length print karo.
+// const friends = ["shafqat", "Hamad", "furqan", "Akmal", "Farman"];
+// console.log(friends.length);
+
+// q5. Ek array me se ek item delete karo.
+// const nums = [1,2,3,4,5,6];
+// nums.splice(1,1);
+// console.log(nums);
