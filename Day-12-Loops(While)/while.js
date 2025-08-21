e1901db2205f86411b399f80f07f39e1f4ca842b
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-21 16:41:03 +0500

    While loop

---
+++ Day-12-Loops(While)/while.js
@@ -0,0 +1,41 @@
+// q1. 1 se 10 tak print karo.
+// let i = 1;
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// q2. 10 se 1 tak reverse print karo.
+// let num = 10;
+// while(num >= 1){
+//     console.log(num);
+//     num--;
+// }
+
+// q3. 1 se 50 tak odd numbers print karo.
+// let num = 1;
+// while(num <= 50){
+//     if(num % 2 !== 0){
+//         console.log(num);
+        
+//     }
+//     num++;
+// }
+
+// q4. Ek array ke sare elements print karo.
+// const colors = ["red", "green", "yellow", "blue", "green"];
+// let index = 0;
+// while(index < colors.length){
+//     console.log(colors[index]);
+//     index++;
+// }
+
+// q5. Ek number ka factorial nikal ke print karo.
+let num = 3;
+let fact = 1;
+while(num > 0){
+    fact = fact * num;
+    num--;
+
+}
+console.log(fact);
