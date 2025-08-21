6a4682f3a5974d0b399e700e54ccb25ce3ed58c4
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-21 16:29:16 +0500

    For loop

---
+++ Day-11-Loops(For)/loops.js
@@ -0,0 +1,31 @@
+// q1. 1 se 10 tak numbers print karo.
+// for (let i = 1; i<=10; i++){
+//     console.log(i);
+// }
+
+// q2. 1 se 20 tak even numbers print karo.
+// for (let i = 1; i<=20; i++){
+//     if (i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+// q3. Ek array ke sare elements print karo.
+// const arr = ["ali", "hanif", "Asif", "tousif", "toufiq", "Arslan"];
+// for (let i = 0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// q4. Ek array ka sum calculate karo.
+// const nums = [1,2];
+// let sum = 0;
+// for (let i = 0; i<nums.length; i++){
+//     sum = sum + nums[i];
+// }
+// console.log(`The sum of array is ${sum}`);
+
+// q5. 5 ka table print karo 
+// const num = 5;
+// for (let i = 1; i<=10; i++){
+//     console.log(`${num} * ${i} = ${num * i}`);
+// }
