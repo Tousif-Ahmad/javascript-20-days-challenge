eb9c2f3f11ec70dd226d94dd9d98c588628dfee3
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-24 00:35:15 +0500

    Function Basics

---
+++ Day-14-Functions(Basics)/index.js
@@ -0,0 +1,35 @@
+// q1. Ek function banao jo “Hello” print kare.
+// function sayhello(){
+//     console.log("Hello");
+// }
+// sayhello()
+
+// q2. Ek function banao jo 2 numbers ka sum return kare.
+// function sum(num1, num2){
+//     return num1 + num2;
+// }
+// console.log(sum(2,3));
+
+// q3. Ek function banao jo ek string ko uppercase me return kare.
+// function uppercase(str){
+//     return str.toUpperCase();
+// }
+// console.log(uppercase("sam"));
+
+// q4. Ek function banao jo ek array ka max value return kare.
+// function maxVal(arr){
+//     return Math.max(...arr);
+// }
+// console.log(maxVal([1,2,3,4,5]));
+
+// q5. Ek function banao jo ek number ka factorial return kare.
+// function factorial(num){
+//     let fact = 1;
+//     for (let i = 1; i<=num; i++){
+//         fact = fact * i;
+//     }
+//     return fact;
+// }
+// console.log(factorial(4));
+
+
