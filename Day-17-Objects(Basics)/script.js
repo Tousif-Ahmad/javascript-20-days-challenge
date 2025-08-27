7a08fec0cce21f06df5d35d5fa4ca60eb8ade10b
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-28 02:26:01 +0500

    Object Basics

---
+++ Day-17-Objects(Basics)/script.js
@@ -0,0 +1,43 @@
+// q1. Ek person object banao jisme name, age, city ho.
+// const person = {
+//     name : "Ufeez",
+//     age : 22,
+//     city : "chitral"
+// }
+
+// q2. Object ke property ko access karo.
+// const person = {
+//     name : "Ufeez",
+//     age : 22,
+//     city : "chitral"
+// }
+// console.log(person.name);
+// console.log(person["city"]);
+
+// q3. Object me ek naya property add karo.
+// const person = {
+//     name : "Ufeez",
+//     age : 22,
+//     city : "chitral"
+// }
+// person.bloodGroup = "B+";
+// console.log(person);
+
+// q4. Object me ek property delete karo.
+// const person = {
+//     name : "Ufeez",
+//     age : 22,
+//     city : "chitral"
+// }
+// delete person.city;
+// console.log(person);
+
+// q5. Object ke sare keys print karo.
+// const person = {
+//     name : "Ufeez",
+//     age : 22,
+//     city : "chitral"
+// }
+// for (const key in person) {
+//     console.log(key);
+// }
