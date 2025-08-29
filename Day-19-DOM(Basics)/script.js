5b72006b0fd09be7882be46a2507f014b203a6d0
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-29 23:15:56 +0500

    DOM Basics

---
+++ Day-19-DOM(Basics)/script.js
@@ -0,0 +1,22 @@
+// q1. Ek heading ka text change karo.
+// const heading = document.getElementById("heading");
+// heading.textContent = "Hello My name is Tousif";
+
+// q2. Ek button click pe alert show karo.
+// const btn = document.querySelector("button");
+// btn.addEventListener("click", function(){
+//     alert("Button clicked")
+// })
+
+// q3. Input box ka value console me print karo.
+// let inp = document.getElementById("inputBox");
+// inp.addEventListener("input", function(e){
+//     console.log(e.target.value);
+// })
+
+// q4. Page ka background color change karo.
+// const btn = document.querySelector("button");
+// btn.addEventListener("click", function(){
+//     document.body.style.backgroundColor = "yellow"
+// })
+
